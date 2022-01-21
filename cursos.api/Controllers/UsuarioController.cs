f63bac6505efcde6cdda2d00eacbfbb21b58483b
Paulo Alencar <paulo.alencar@fatec.sp.gov.br>
2022-01-21 10:33:45 -0300

    Criação da Validação ModelState customizado com o conceito de filtros

--- cursos.api/Controllers/UsuarioController.cs
+++ cursos.api/Controllers/UsuarioController.cs
@@ -1,3 +1,4 @@
+using cursos.api.Filters;
 using cursos.api.Models;
 using cursos.api.Models.Usuarios;
 using Microsoft.AspNetCore.Mvc;
@@ -21,20 +22,18 @@ namespace cursos.api.Controllers
 		[SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
 		[HttpPost]
 		[Route("logar")]
+		[ValidacaoModelStateCustomizado]
 		public IActionResult Logar(LoginViewModelInput loginViewModelInput)
 		{
-            if (!ModelState.IsValid)
-            {
-				return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
-            }
+
 			return Ok(loginViewModelInput);
 		}
 
 		[HttpPost]
 		[Route("registrar")]
+		[ValidacaoModelStateCustomizado]
 		public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
 		{
-
 			return Created("", registroViewModelInput);
 		}
 
---
+++ cursos.api/Filters/ValidacaoModelStateCustomizado.cs
@@ -0,0 +1,22 @@
+﻿using cursos.api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace cursos.api.Filters
+{
+    public class ValidacaoModelStateCustomizado : ActionFilterAttribute
+    {
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var validaCampoViewModel = new ValidaCampoViewModelOutput(context.ModelState
+                                                                            .SelectMany(sm => sm.Value.Errors)
+                                                                            .Select(s => s.ErrorMessage));
+                context.Result = new BadRequestObjectResult(validaCampoViewModel);
+            }
+        }
+    }
+}
